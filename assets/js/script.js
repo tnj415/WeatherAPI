22bba6f6663b26c4c0822acf87f93d99391197d6
Troy Johnson <tnj8510@gmail.com>
2021-07-04 21:04:09 -0700

    finished 5 day forecast, added background img

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+var bgroundImg = document.querySelector("background-img")
 var cityEl = document.querySelector(".city")
 var dateEl = document.querySelector(".date")
 var iconEl = document.querySelector(".w-icon")
@@ -14,21 +15,22 @@ var ss2DateEl = document.querySelector(".ss2-date")
 var ss3DateEl = document.querySelector(".ss3-date")
 var ss4DateEl = document.querySelector(".ss4-date")
 var ss5DateEl = document.querySelector(".ss5-date")
-var ss1ImgEl = document.querySelector("ss1-img")
-var ss2ImgRl = document.querySelector("ss2-img")
-var ss3ImgEl = document.querySelector("ss3-img")
-var ss4ImgEl = document.querySelector("ss4-img")
-var ss5ImgEl = document.querySelector("ss5-img")
-var ss1TempEl = document.querySelector("ss1-temp")
-var ss2TempEl = document.querySelector("ss2-temp")
-var ss3TempEl = document.querySelector("ss3-temp")
-var ss4TempEl = document.querySelector("ss4-temp")
-var ss5TempEl = document.querySelector("ss5-temp")
-var ss1HumEl = document.querySelector("ss1-hum")
-var ss2HumEl = document.querySelector("ss2-hum")
-var ss3HumEl = document.querySelector("ss3-hum")
-var ss4HumEl = document.querySelector("ss4-hum")
-var ss5HumEl = document.querySelector("ss5-hum")
+var ss1ImgEl = document.querySelector(".ss1-img")
+var ss2ImgEl = document.querySelector(".ss2-img")
+var ss3ImgEl = document.querySelector(".ss3-img")
+var ss4ImgEl = document.querySelector(".ss4-img")
+var ss5ImgEl = document.querySelector(".ss5-img")
+var ss1TempEl = document.querySelector(".ss1-temp")
+var ss2TempEl = document.querySelector(".ss2-temp")
+var ss3TempEl = document.querySelector(".ss3-temp")
+var ss4TempEl = document.querySelector(".ss4-temp")
+var ss5TempEl = document.querySelector(".ss5-temp")
+var ss1HumEl = document.querySelector(".ss1-hum")
+var ss2HumEl = document.querySelector(".ss2-hum")
+var ss3HumEl = document.querySelector(".ss3-hum")
+var ss4HumEl = document.querySelector(".ss4-hum")
+var ss5HumEl = document.querySelector(".ss5-hum")
+
 
 var apiKey = "28ef248f0eebeeb867508af6072a7c3e"
 
@@ -43,6 +45,8 @@ function searchCity(e) {
 
 function getAPI1(city) {
 
+    document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + city + "')"
+    
     fetch("https://api.openweathermap.org/data/2.5/weather?q="
         + city
         + "&units=imperial&appid="
@@ -107,7 +111,9 @@ function displayWeather1(data) {
     iconEl.src = "http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png"
     tempEl.innerText = "Temperature: " + data.main.temp + "°F";
     humEl.innerText = "Humidity: " + data.main.humidity + "%";
-    wsEl.innerText = "Wind Speed: " + data.wind.speed + "MPH"
+    wsEl.innerText = "Wind Speed: " + data.main.speed + "MPH"
+
+    
 }
 
 function displayWeather2(data) {
@@ -131,20 +137,58 @@ function displayWeather2(data) {
 // function setDate () {
 
 
-// }
-
-// function displayFiveDayForecast(data) {
-
-//     console.log("5df = ", data)
-
-//     ss1DateEl.innerText = moment().format("MM/DD/YYYY")
-
-//     ss1ImgEl.src = "http://openweathermap.org/img/wn/" +  + ".png"
-
-//     ss1TempEl.innerText = "Temp: " + + "°F"
-
-//     ss1HumEl.innerText = "Humidity: " + + "%"
 
 
+function displayFiveDayForecast(data) {
 
-// }
+    console.log("5df = ", data)
+
+    //snapshot1
+    ss1DateEl.innerText = moment().format("MM/DD/YYYY")
+
+    ss1ImgEl.src = "http://openweathermap.org/img/wn/" + data.daily[0].weather[0].icon + ".png"
+
+    ss1TempEl.innerText = "Temp: " + data.daily[0].temp.day + "°F"
+
+    ss1HumEl.innerText = "Humidity: " + data.daily[0].humidity + "%"
+
+    //snapshot2
+    ss2DateEl.innerText = moment().format("MM/DD/YYYY")
+
+    ss2ImgEl.src = "http://openweathermap.org/img/wn/" + data.daily[1].weather[0].icon + ".png"
+
+    ss2TempEl.innerText = "Temp: " + data.daily[1].temp.day + "°F"
+
+    ss2HumEl.innerText = "Humidity: " + data.daily[1].humidity + "%"
+
+    //snapshot3
+    ss3DateEl.innerText = moment().format("MM/DD/YYYY")
+
+    ss3ImgEl.src = "http://openweathermap.org/img/wn/" + data.daily[2].weather[0].icon + ".png"
+
+    ss3TempEl.innerText = "Temp: " + data.daily[2].temp.day + "°F"
+
+    ss3HumEl.innerText = "Humidity: " + data.daily[2].humidity + "%"
+
+    //snapshot4
+    ss4DateEl.innerText = moment().format("MM/DD/YYYY")
+
+    ss4ImgEl.src = "http://openweathermap.org/img/wn/" + data.daily[3].weather[0].icon + ".png"
+
+    ss4TempEl.innerText = "Temp: " + data.daily[3].temp.day + "°F"
+
+    ss4HumEl.innerText = "Humidity: " + data.daily[3].humidity + "%"
+
+    //snapshot5
+    ss5DateEl.innerText = moment().format("MM/DD/YYYY")
+
+    ss5ImgEl.src = "http://openweathermap.org/img/wn/" + data.daily[4].weather[0].icon + ".png"
+
+    ss5TempEl.innerText = "Temp: " + data.daily[4].temp.day + "°F"
+
+    ss5HumEl.innerText = "Humidity: " + data.daily[4].humidity + "%"
+
+
+
+
+}
